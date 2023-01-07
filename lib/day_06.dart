a18433dfbc7e9763b1c9639d650a4d44435b80ac
Adam Kiss <iam@adamkiss.com>
2023-01-07 05:48:40 +0100

    Add tuple & finish day 06/a

--- lib/day_06.dart
+++ lib/day_06.dart
@@ -2,44 +2,47 @@ import 'dart:collection';
 import 'dart:developer';
 import 'dart:io';
 
+import 'package:tuple/tuple.dart';
+
 Iterable<String> _input() => File('inputs/2015/day_06.txt').readAsLinesSync();
 
-List<Map<int, int>> _area_to_list(int startX, int startY, int endX, int endY) {
-	List<Map<int, int>> result = [];
+List<Tuple2<int,int>> _area_to_list(int startX, int startY, int endX, int endY) {
+	List<Tuple2<int,int>> result = [];
 	for (var j = startY; j <= endY; j++) {
-		result.add({
-			0: j * 1000 + startX,
-			1: j * 1000 + endX
-		});
+		result.add(Tuple2(j * 1000 + startX, j * 1000 + endX + 1)); // +1 changes range inclusive -> exclusive
 	}
 	return result;
 }
 
 void main() {
 	/* PART A: RUN 1 */
-	var lights = List<bool>.filled(1000000, false, growable: false);
+	var lights = List<bool>.filled(1000000, false);
 	final parserA = RegExp(r'^(?<action>.*) (?<sX>\d+),(?<sY>\d+) through (?<eX>\d+),(?<eY>\d+)');
 	_input()
-		.where((element) => parserA.hasMatch(element))
 		.map((e) {
 			final m = parserA.firstMatch(e);
-			return [
-				m?.namedGroup('action'),
+			return Tuple2<String,List>(
+				m!.namedGroup('action')!,
 				_area_to_list(
-					int.parse(m?.namedGroup('sX') ?? '0', radix: 10), int.parse(m?.namedGroup('sY') ?? '0', radix: 10),
-					int.parse(m?.namedGroup('eX') ?? '0', radix: 10), int.parse(m?.namedGroup('eY') ?? '0', radix: 10)
+					int.parse(m.namedGroup('sX')!, radix: 10), int.parse(m.namedGroup('sY')!, radix: 10),
+					int.parse(m.namedGroup('eX')!, radix: 10), int.parse(m.namedGroup('eY')!, radix: 10)
 				)
-			];
+			);
 		})
-		.forEach((m) {
-			if (m[0].startsWith('turn')) {
-				m['ranges']?.cast<List<Map<int,int>>>().forEach((range) {
-					// fill range with tru or false
-					lights.fillRange(range[0] ?? 0, range[1] ?? 0, (m['action'] as String) == 'turn on');
-				});
+		.forEach((Tuple2<String, List> m) {
+			if (m.item1.startsWith('turn')) {
+				for (final range in m.item2) {
+					lights.fillRange(range.item1, range.item2, m.item1 == 'turn on');
+				}
+				return;
 			}
-			// replace range with switch ranged
-			// lights.replaceRange(range[0], range[1], lights.slice().map(e => ! e))
+
+			for (final range in m.item2) {
+				lights.setRange(
+					range.item1, range.item2,
+					lights.sublist(range.item1, range.item2).map((e) => !e)
+				);
+			}
 		});
 	stdout.write(' A: ${lights.where((l) => l == true).length}');
 
