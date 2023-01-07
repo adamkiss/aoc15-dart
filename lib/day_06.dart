a819fdc7b1904a7710f371caa402425a3dabea20
Adam Kiss <iam@adamkiss.com>
2023-01-07 06:01:43 +0100

    Finish day 6

--- lib/day_06.dart
+++ lib/day_06.dart
@@ -1,6 +1,7 @@
 import 'dart:collection';
 import 'dart:developer';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:tuple/tuple.dart';
 
@@ -15,38 +16,51 @@ List<Tuple2<int,int>> _area_to_list(int startX, int startY, int endX, int endY) 
 }
 
 void main() {
-	/* PART A: RUN 1 */
-	var lights = List<bool>.filled(1000000, false);
-	final parserA = RegExp(r'^(?<action>.*) (?<sX>\d+),(?<sY>\d+) through (?<eX>\d+),(?<eY>\d+)');
-	_input()
-		.map((e) {
-			final m = parserA.firstMatch(e);
-			return Tuple2<String,List>(
-				m!.namedGroup('action')!,
-				_area_to_list(
-					int.parse(m.namedGroup('sX')!, radix: 10), int.parse(m.namedGroup('sY')!, radix: 10),
-					int.parse(m.namedGroup('eX')!, radix: 10), int.parse(m.namedGroup('eY')!, radix: 10)
-				)
-			);
-		})
-		.forEach((Tuple2<String, List> m) {
-			if (m.item1.startsWith('turn')) {
-				for (final range in m.item2) {
-					lights.fillRange(range.item1, range.item2, m.item1 == 'turn on');
-				}
-				return;
-			}
+	final parser = RegExp(r'^(?<action>.*) (?<sX>\d+),(?<sY>\d+) through (?<eX>\d+),(?<eY>\d+)');
+	final parsed_instructions = _input().map((e) {
+		final m = parser.firstMatch(e);
+		return Tuple2<String,List>(
+			m!.namedGroup('action')!,
+			_area_to_list(
+				int.parse(m.namedGroup('sX')!, radix: 10), int.parse(m.namedGroup('sY')!, radix: 10),
+				int.parse(m.namedGroup('eX')!, radix: 10), int.parse(m.namedGroup('eY')!, radix: 10)
+			)
+		);
+	});
 
-			for (final range in m.item2) {
-				lights.setRange(
-					range.item1, range.item2,
-					lights.sublist(range.item1, range.item2).map((e) => !e)
-				);
+	/* PART A: RUN 1 */
+	var lightsA = List<bool>.filled(1000000, false);
+	for (final Tuple2<String, List> instr in parsed_instructions) {
+		if (instr.item1.startsWith('turn')) {
+			for (final range in instr.item2) {
+				lightsA.fillRange(range.item1, range.item2, instr.item1 == 'turn on');
 			}
-		});
-	stdout.write(' A: ${lights.where((l) => l == true).length}');
+			continue;
+		}
+
+		for (final range in instr.item2) {
+			lightsA.setRange(
+				range.item1, range.item2,
+				lightsA.sublist(range.item1, range.item2).map((e) => !e)
+			);
+		}
+	}
+	stdout.write(' A: ${lightsA.where((l) => l == true).length}');
 
 	/* PART B: RUN 1 */
-
-	stdout.write(' B: ${false}');
+	var lightsB = List<int>.filled(1000000, 0);
+	final Map<String, int> changeB = {
+		'turn on' : 1,
+		'turn off': -1,
+		'toggle'  : 2
+	};
+	for (final Tuple2<String, List> instr in parsed_instructions) {
+		for (final range in instr.item2) {
+			lightsB.setRange(
+				range.item1, range.item2,
+				lightsB.sublist(range.item1, range.item2).map((e) => max(0, e + (changeB[instr.item1] ?? 0)))
+			);
+		}
+	}
+	stdout.write(' B: ${lightsB.reduce((total, curr) => curr + total)}');
 }
